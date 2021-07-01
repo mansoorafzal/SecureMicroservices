2f29210a79e6c21846511673b879e6e777dac952
mansoorafzal <mansoorafzal@gmail.com>
2021-07-01 13:40:34 +0400

    Identity Server project setup

---
+++ src/IdentityServer/Config.cs
@@ -0,0 +1,39 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                   
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+           new ApiScope[]
+           {
+               
+           };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+          new ApiResource[]
+          {
+               
+          };
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+          new IdentityResource[]
+          {
+              
+          };
+
+        public static List<TestUser> TestUsers =>
+            new List<TestUser>
+            {
+                
+            };
+    }
+}
