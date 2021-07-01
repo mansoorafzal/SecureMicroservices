7a36aad0c16c9b50c585cb05e151402932e34d3c
mansoorafzal <mansoorafzal@gmail.com>
2021-07-01 19:25:19 +0400

    Movies. Client project setup

---
+++ src/Movies.Client/Controllers/MoviesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Movies.Client.Models;
+using Movies.Client.Services;
+
+namespace Movies.Client.Controllers
+{
+    public class MoviesController : Controller
+    {   
+        private readonly IMovieService _movieService;
+
+        public MoviesController(IMovieService movieService)
+        {
+            _movieService = movieService ?? throw new ArgumentNullException(nameof(movieService));
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            return View(await _movieService.GetMovies());
+        }
+
+        // GET: Movies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        // GET: Movies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Genre,Rating,ReleaseDate,ImageUrl,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        // GET: Movies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            return View();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/Movies.Client/Services/IMovieService.cs
@@ -0,0 +1,19 @@
+﻿using Movies.Client.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Services
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        
+        Task<Movie> GetMovie(string id);
+        
+        Task<Movie> CreateMovie(Movie movie);
+        
+        Task<Movie> UpdateMovie(Movie movie);
+        
+        Task DeleteMovie(int id);
+    }
+}
